4ba58548360535bf4328b8e6ab48c16a044854dc
jake <jakemyers2016@u.northwestern.edu>
2020-04-18 16:48:19 -0400

    j

--- auth/main.py
+++ auth/main.py
@@ -4,18 +4,6 @@ import config
 
 auth_api = Blueprint('auth_api', __name__, )
 
-@auth_api.route('/getCode', methods=['GET'])
-def getCode():
-    redirectUri = request.args.get('redirect_uri', '')
-    payload = {
-        'client_id': config.spotify['CLIENT_ID'],
-        'response_type': 'code',
-        'redirect_uri': redirectUri or f'{config.URL}/auth/getTokens',
-        'scopes': 'user-read-private user-read-email'
-    }
-    res = requests.get('https://accounts.spotify.com/authorize', params=payload)
-    return redirect(res.url, code=302)
-
 @auth_api.route('/getTokens', methods=['GET'])
 def getTokens():
     authCode = request.args.get('code', '')
