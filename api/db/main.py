3fd46eb2c77ee46798d2b42bd0964fa4a57ccfeb
jmm526 <jakemyers2016@u.northwestern.edu>
2020-04-24 13:31:30 -0400

    added user functionality on backend

---
+++ api/db/main.py
@@ -0,0 +1,86 @@
+import config
+import os
+from flask import Blueprint,request,abort
+from firebase_admin import credentials,initialize_app
+import mock
+from google.cloud import firestore
+import google.auth.credentials
+import json
+import requests
+import datetime
+
+
+if config.APP_ENV == 'Production':
+    db = firestore.Client()
+else:
+    # TODO: differentiate test and integration environments (or just add flag to write to local mock database)
+    # os.environ["FIRESTORE_DATASET"] = "test"
+    # os.environ["FIRESTORE_EMULATOR_HOST"] = "localhost:8001"
+    # os.environ["FIRESTORE_EMULATOR_HOST_PATH"] = "localhost:8001/firestore"
+    # os.environ["FIRESTORE_HOST"] = "http://localhost:8001"
+    # os.environ["FIRESTORE_PROJECT_ID"] = "test"
+    #
+    # credentials = mock.Mock(spec=google.auth.credentials.Credentials)
+    # db = firestore.Client(project="test", credentials=credentials)
+    cred = credentials.Certificate('iconic-hue-273619-firebase-adminsdk-vomu4-f1c9d6a6bb.json')
+    initialize_app(cred)
+    db = firestore.Client()
+
+user_routes = Blueprint('user_routes', __name__)
+
+usersRef = db.collection(config.USERS_COLLECTION_ID)
+
+@user_routes.route('/getUser', methods=['GET'])
+def getUser():
+    userId = request.args.get('id')
+    if userId:
+        return json.dumps(usersRef.document(userId).get().to_dict())
+    else:
+        return "User not found.", 404
+
+@user_routes.route('/createUser', methods=['POST'])
+def createUser():
+    try:
+        authCode = request.form.get('code', '')
+        redirectUri = request.form.get('redirect_uri', '')
+        payload = {'code': authCode, 'redirect_uri': redirectUri}
+        tokensRes = requests.get(f'{config.URL}/auth/getTokens', params=payload)
+        tokens = tokensRes.json()
+        userRes = requests.get(f'{config.URL}{config.API_PREFIX}/spotify/me', params={'access_token': tokens.get('access_token'), 'refresh_token': tokens.get('refresh_token')})
+        user = userRes.json()
+
+        # only runs when user is found
+        dbUser = usersRef.where(u'spotify_id', u'==', user['id']).stream()
+        for u in dbUser:
+            updateArgs = {'id': u.id}
+            updatePayload = {
+                'spotify_auth_code': authCode,
+                'spotify_access_token': tokens['access_token'],
+                'spotify_refresh_token': tokens['refresh_token']
+            }
+            requests.put(f'{config.URL}{config.API_PREFIX}/users/updateUser', params=updateArgs, data=updatePayload)
+            return json.dumps({'id': u.id})
+
+        # else create user
+        userRef = usersRef.document()
+        userRef.set({
+            'is_live': False,
+            'spotify_access_token': tokens['access_token'],
+            'spotify_auth_code': authCode,
+            'spotify_id': user['id'],
+            'spotify_display_name': user['display_name'],
+            'spotify_refresh_token': tokens['refresh_token'],
+            'created_at': datetime.datetime.now().strftime('%Y-%d-%mT%H:%M:%S')
+        })
+        return json.dumps({'id': userRef.id})
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+@user_routes.route('/updateUser', methods=['PUT', 'POST'])
+def updateUser():
+    try:
+        userId = request.args.get('id')
+        usersRef.document(userId).update(request.form)
+        return json.dumps({'success': True})
+    except Exception as e:
+        return f"An Error Occured: {e}"
--- config/settings.py
+++ config/settings.py
@@ -1,35 +1,31 @@
 from config.getSecret import getSecret
 
-# SECRET ID's
-
-API_PREFIX = 'API_PREFIX'
-SPOTIFY_API_URL = 'SPOTIFY_API_URL'
-SPOTIFY_CLIENT_ID = 'SPOTIFY_CLIENT_ID'
-SPOTIFY_CLIENT_SECRET = 'SPOTIFY_CLIENT_SECRET'
-
 class BaseConfig():
-   API_PREFIX = getSecret(API_PREFIX)
-   TESTING = False
-   DEBUG = False
-   spotify = {
-        'API_URL': getSecret(SPOTIFY_API_URL),
-        'CLIENT_ID': getSecret(SPOTIFY_CLIENT_ID),
-        'CLIENT_SECRET': getSecret(SPOTIFY_CLIENT_SECRET),
-   }
+    API_PREFIX = getSecret('API_PREFIX')
+    TESTING = False
+    DEBUG = False
+    spotify = {
+        'API_URL': getSecret('SPOTIFY_API_URL'),
+        'CLIENT_ID': getSecret('SPOTIFY_CLIENT_ID'),
+        'CLIENT_SECRET': getSecret('SPOTIFY_CLIENT_SECRET'),
+    }
+    FIRESTORE_KEY = getSecret('FIRESTORE_KEY')
 
 
 class DevConfig(BaseConfig):
-   FLASK_ENV = 'development'
-   DEBUG = True
-   URL = 'http://127.0.0.1:8080'
+    FLASK_ENV = 'development'
+    DEBUG = True
+    URL = 'http://127.0.0.1:8080'
+    USERS_COLLECTION_ID = getSecret('USERS_COLLECTION_ID_DEV')
 
 
 class ProductionConfig(BaseConfig):
-   FLASK_ENV = 'production'
-   URL = 'https://iconic-hue-273619.appspot.com'
+    FLASK_ENV = 'production'
+    URL = 'https://iconic-hue-273619.appspot.com'
+    USERS_COLLECTION_ID = getSecret('USERS_COLLECTION_ID_PRODUCTION')
 
 
 class TestConfig(BaseConfig):
-   FLASK_ENV = 'development'
-   TESTING = True
-   DEBUG = True
+    FLASK_ENV = 'development'
+    TESTING = True
+    DEBUG = True
